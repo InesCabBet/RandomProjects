efbf2a91b14ef829b82f89fde03f87bd1b364f18
InesCabBet <icb3rg@gmail.com>
2026-04-02 13:12:03 +0100

    puntos

--- Monopoly/dices.py
+++ Monopoly/dices.py
@@ -8,7 +8,22 @@ screen.fill((0,0,0))
 def draw_dice(screen, x, y, number, size=100):
   py.draw.rect(screen, (0, 0, 0), (x,y,size,size))
   py.draw.rect(screen,(255,255,255), (x, y, size, size))
-  do_radius = 8
+  dot_radius = 8
+  center_x = x + size // 2
+  center_y = y + size // 2
+  margin = size //5
+
+  positions = {
+    1 : [(center_x, center_y)],
+    2: [(x + margin, y + margin), (x + size - margin, y + size - margin)],
+    3: [(x + margin, y + margin), (center_x, center_y), (x + size - margin, y + size - margin)],
+    4: [(x + margin, y + margin), (x + size - margin, y + margin), (x + margin, y + size - margin), (x + size - margin, y + size - margin)],
+    5: [(x + margin, y + margin), (x + size - margin, y + margin), (center_x, center_y), (x + margin, y + size - margin), (x + size - margin, y + size - margin)],
+    6: [(x + margin, y + margin), (x + margin, center_y), (x + margin, y + size - margin), (x + size - margin, y + margin), (x + size - margin, center_y), (x + size - margin, y + size - margin)]
+  }
+
+  for pos in positions.get(number, []):
+    py.draw.circle(screen, (255, 255, 255), pos, dot_radius)
 
 def roll_dice():
   rolls = np.random.randint(low=1, high=6, size=2)
