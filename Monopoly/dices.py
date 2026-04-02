69683fbc194a01f3f02495bdfaae518236ffcd75
InesCabBet <icb3rg@gmail.com>
2026-04-02 13:22:53 +0100

    avances

--- Monopoly/dices.py
+++ Monopoly/dices.py
@@ -2,7 +2,7 @@ import numpy as np
 import pygame as py
 
 py.init()
-screen = py.display.set_mode((800,600))
+screen = py.display.set_mode((400,400))
 screen.fill((0,0,0))
 
 def draw_dice(screen, x, y, number, size=100):
@@ -14,7 +14,7 @@ def draw_dice(screen, x, y, number, size=100):
   margin = size //5
 
   positions = {
-    1 : [(center_x, center_y)],
+    1: [(center_x, center_y)],
     2: [(x + margin, y + margin), (x + size - margin, y + size - margin)],
     3: [(x + margin, y + margin), (center_x, center_y), (x + size - margin, y + size - margin)],
     4: [(x + margin, y + margin), (x + size - margin, y + margin), (x + margin, y + size - margin), (x + size - margin, y + size - margin)],
@@ -23,18 +23,21 @@ def draw_dice(screen, x, y, number, size=100):
   }
 
   for pos in positions.get(number, []):
-    py.draw.circle(screen, (255, 255, 255), pos, dot_radius)
+    py.draw.circle(screen, (0, 0, 0), pos, dot_radius)
 
 def roll_dice():
   rolls = np.random.randint(low=1, high=6, size=2)
   return rolls
 
 running = True
+rolls = roll_dice()
 while running:
   for event in py.event.get():
     if event.type == py.QUIT:
       running = False
   screen.fill((0, 0, 0))
+  draw_dice(screen, 50, 130, rolls[0])
+  draw_dice(screen, 250, 130, rolls[1])
   py.display.flip()
 py.quit()
 
