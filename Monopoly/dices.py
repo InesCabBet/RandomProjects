7031c3511411a4374c90ad485b20326cb4741c7e
InesCabBet <icb3rg@gmail.com>
2026-04-02 22:21:39 +0100

    monopoly acabado

--- Monopoly/dices.py
+++ Monopoly/dices.py
@@ -4,6 +4,7 @@ import pygame as py
 py.init()
 screen = py.display.set_mode((400,400))
 screen.fill((0,0,0))
+py.display.set_caption('Dados del Monopoly')
 
 def draw_dice(screen, x, y, number, size=100):
   py.draw.rect(screen, (0, 0, 0), (x,y,size,size))
@@ -29,6 +30,31 @@ def roll_dice():
   rolls = np.random.randint(low=1, high=6, size=2)
   return rolls
 
+class Button:
+  def __init__(self, text, x, y, enable):
+    self.text = text
+    self.x_pos = x
+    self.y_pos = y
+    self.enabled = enable
+    self.draw()
+
+  def draw(self):
+    font = py.font.Font(None, 32)
+    button_text = font.render(self.text, True, 'black')
+    button_rec = py.rect.Rect((self.x_pos, self.y_pos), (100,30))
+    py.draw.rect(screen, 'white', button_rec, 0, 5)
+    py.draw.rect(screen, 'gray', button_rec, 2, 5)
+    screen.blit(button_text, (self.x_pos + 3, self.y_pos + 3))
+
+  def check_click(self):
+    mouse_pos = py.mouse.get_pos()
+    left_click = py.mouse.get_pressed()[0]
+    button_rect = py.rect.Rect((self.x_pos, self.y_pos), (100,30))
+    if left_click and button_rect.collidepoint(mouse_pos) and self.enabled:
+      return True
+    else :
+      return False
+
 running = True
 rolls = roll_dice()
 while running:
@@ -36,9 +62,13 @@ while running:
     if event.type == py.QUIT:
       running = False
   screen.fill((0, 0, 0))
+  my_button = Button('Roll Dice', 150, 300, True)
+  if my_button.check_click():
+    rolls = roll_dice()
   draw_dice(screen, 50, 130, rolls[0])
   draw_dice(screen, 250, 130, rolls[1])
   py.display.flip()
+  py.display.update()
 py.quit()
 
 
